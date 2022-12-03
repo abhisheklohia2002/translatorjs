b63aee1a10e61442cfc75d4c47850532e9c49673
abhisheklohia2002 <abhisheklohia46458@gmail.com>
2022-12-03 20:47:36 +0530

    added

---
+++ js.js
@@ -0,0 +1,24 @@
+const countries = 
+    [
+        {"name": "Amharic", "code": "am"},
+        {"name": "Arabic", "code": "ar"},
+        {"name": "Basque", "code": "eu"},
+        {"name": "Bengali", "code": "bn"},
+        {"name": "English", "code": "en-GB"},
+        {"name": "Portuguese", "code": "pt-BR"},
+        {"name": "Bulgarian", "code": "bg"},
+        {"name": "catalan", "code": "ca"},
+        {"name": "Cherokee", "code": "chr"},
+        {"name": "Croatian", "code": "hr"},
+        {"name": "Czech", "code": "cs"},
+        {"name": "Danish", "code": "da"},
+        {"name": "German", "code": "de"},
+        {"name": "Hindi", "code": "hi"},
+        {"name": "Greek", "code": "el"},
+        {"name": "French", "code": "fr"},
+        {"name": "Gujarati", "code": "gu"},
+    ]
+
+
+      export default countries;
+      
